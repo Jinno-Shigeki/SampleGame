fe47667f02d6547becbfdec15745ade7c8181f73
Jinno-Shigeki <booboo080yamaya@gmail.com>
2022-12-03 21:21:58 +0900

    プレハブ直す

--- Assets/Scripts/GameManeger/GameManager.cs
+++ Assets/Scripts/GameManeger/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
-using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
